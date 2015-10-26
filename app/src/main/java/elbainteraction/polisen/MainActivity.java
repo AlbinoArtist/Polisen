3c7a971b8fdd95253c0da90d510eb98c29e434ae
AlbinoArtist <henrikpaoledlund@hotmail.com>
2015-10-26 12:46:23 +0100

    1st

--- app/src/main/java/elbainteraction/polisen/MainActivity.java
+++ app/src/main/java/elbainteraction/polisen/MainActivity.java
@@ -2,6 +2,9 @@ package elbainteraction.polisen;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.xiaochen.progressroundbutton.AnimDownloadProgressButton;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Hora(View view){
+        AnimDownloadProgressButton b = (AnimDownloadProgressButton) view;
+        b.setText("hora");
+        b.setState(1);
     }
 }
